829df8eec48b66072ba9a09b0d4962e5550ac810
Premraj Patil <ppremraj697@gmail.com>
2024-07-25 22:43:26 +0530

    Implemented OTP resend countdown timer using useEffect() hook

--- components/OtpLogin.tsx
+++ components/OtpLogin.tsx
@@ -1,4 +1,42 @@
+import { auth } from "@/firebase"
+import {
+  ConfirmationResult,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth"
+import React, { FormEvent, use, useEffect, useState, useTransition } from "react";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 const OtpLogin = () => {
+
+  const router = useRouter();
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState("");
+  const [resendCountdown, setResendCountdown] = useState(0);
+  const [recaptchaVerifier, setRecaptchaVerifier] = useState<RecaptchaVerifier | null>(null);
+  const [confirmationResult, setConfirmationResult] = useState<ConfirmationResult | null>(null);
+
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (resendCountdown > 0) {
+      timer = setTimeout(() => setResendCountdown(resendCountdown - 1), 1000)
+    }
+    return () => clearTimeout(timer);
+  }, [resendCountdown]);
+
   return (
     <div>
       OTP Login Component
